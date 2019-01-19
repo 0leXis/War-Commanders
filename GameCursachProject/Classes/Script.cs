8b9fe9ee6d0c784321581ed1c5e639c02e5c5a8e
0leXis <alexbashur@gmail.com>
2019-01-19 19:32:05 +0300

    Lua Scripts

---
+++ GameCursachProject/Classes/Script.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLua;
+
+namespace GameCursachProject.Classes
+{
+    class Script
+    {
+        private Lua LuaScript;
+
+        public string TextScript { get; set; }
+
+        public Script(string TextScript_FileName, bool IsFromFile = false)
+        {
+            LuaScript = new Lua();
+            LuaScript.LoadCLRPackage();
+
+            if (IsFromFile)
+                TextScript = TextScript_FileName;
+            else
+                using (var Fil = new StreamReader(TextScript_FileName, Encoding.Default))
+                    TextScript = Fil.ReadToEnd();
+        }
+
+        public object[] DoScript()
+        {
+            return LuaScript.DoString(TextScript);
+        }
+
+        public void RegisterVar(ref object Obj, string VarName)
+        {
+            LuaScript[VarName] = Obj;
+        }
+
+        public object GetVar(string VarName)
+        {
+            return LuaScript[VarName];
+        }
+
+        public LuaFunction GetFunc(string FuncName)
+        {
+            return LuaScript[FuncName] as LuaFunction;
+        }
+    }
+}
